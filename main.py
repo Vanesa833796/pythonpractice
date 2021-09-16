326ad908dc0f22521571c37df1a29bd9c118ef40
Vanesa833796 <833796@mystma.org>
2021-09-16 09:18:32 -0500

    function practice

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#define function
 def topFiveMovies():
     #print first movie
     print("Fast and Furious")
@@ -16,3 +17,100 @@ def topFiveMovies():
 
 print("these are my top five movies.")
 topFiveMovies()
+
+#define function
+def topFiveFoods():
+    #print first food
+    print("Carbonara")
+ 
+    #print second food
+    print("Zuppa Toscana")
+ 
+    #print third food
+    print("Pizza")
+ 
+    #print fourth food
+    print("French fries")
+   
+    #print fifth food
+    print("Tacos")
+print("these are my top five foods.")
+topFiveFoods()
+
+#define function
+def topFiveColors():
+    #print first color
+    print("Sage")
+ 
+    #print second color
+    print("Red")
+ 
+    #print third color
+    print("Black")
+ 
+    #print fourth color
+    print("Light Blue")
+   
+    #print fifth color
+    print("White")
+print("these are my top five colors.")
+topFiveColors()
+
+#define function
+def topFiveDreamVacations():
+    #print first vacation
+    print("Italy")
+ 
+    #print second vacation
+    print("Switzerland")
+ 
+    #print third vacation
+    print("Greece")
+ 
+    #print fourth vacation
+    print("Maldives")
+   
+    #print fifth vacation
+    print("Fiji")
+print("these are my top five dream vacations.")
+topFiveDreamVacations()
+
+#define a function that outputs your name
+#followed by "is my name"
+def myNameIs(turtle):
+   
+    #output strings
+    print(turtle + " is my  name")
+
+    #run function
+    myNameIs("Vanesa")
+
+#define function
+def timesTwo(heart):
+
+    #output
+    print(heart * 2)
+
+timesTwo(30)
+
+#define fumction
+def plusFive(pizza):
+
+    #output
+    print(pizza + 5)
+
+plusFive(14)
+
+#define function
+def divByTwo(sage):
+
+    #output
+    print(sage / 2)
+
+divByTwo(6)
+
+
+def addTwoNumbers(x, y):
+    print(x + y)
+
+addTwoNumbers(10,59)
