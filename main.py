eea23fae4b261956e073c5fa2f0af87df3216136
Vanesa833796 <833796@mystma.org>
2021-09-20 09:01:07 -0500

    deleted some things(added something new)

--- main.py
+++ main.py
@@ -1,60 +1,5 @@
-#define function
-def topFiveMovies():
-    #print first movie
-    print("Fast and Furious")
-  
-    #print second movie
-    print("Pirates of the Caribbean")
-    
-    #print third movie
-    print("Terminator")
-  
-    #print fourth movie
-    print("The Hunger Games")
-    
-    #print fifth movie
-    print("Harry Potter")
 
-print("these are my top five movies.")
-topFiveMovies()
 
-#define function
-def topFiveFoods():
-    #print first food
-    print("Carbonara")
- 
-    #print second food
-    print("Zuppa Toscana")
- 
-    #print third food
-    print("Pizza")
- 
-    #print fourth food
-    print("French fries")
-   
-    #print fifth food
-    print("Tacos")
-print("these are my top five foods.")
-topFiveFoods()
-
-#define function
-def topFiveColors():
-    #print first color
-    print("Sage")
- 
-    #print second color
-    print("Red")
- 
-    #print third color
-    print("Black")
- 
-    #print fourth color
-    print("Light Blue")
-   
-    #print fifth color
-    print("White")
-print("these are my top five colors.")
-topFiveColors()
 
 #define function
 def topFiveDreamVacations():
@@ -122,4 +67,18 @@ def madLib(a,b,c,d,e,f,g,h,i,j,k):
     print("Star Wars is a" + a + b + "of" +c + "versus evil in a" + d + "far far away." + "There are" + e + "battles between" + f + g + "in" + h + "space and" + i + "duels with" + j + "called" + k + "sabers.")
 
 madLib(" fluffy "," cat "," short "," school "," colorful "," cold "," cars "," big "," pretty "," dogs "," ugly ")
+
+#define function
+def woof():
+
+    #output
+    print("woof")
+
+woof()
+
+def woof():
+
+    return "woof"
+
+    woof()
 
